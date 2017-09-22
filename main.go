74a66dd9363dcd9dbea7ddeb1b4f2e81bf08fa58
myth <myth@overflow.no>
2017-09-22 21:55:08 +0200

    Add more example code
    Integrated the Fosite examples into main

--- main.go
+++ main.go
@@ -4,13 +4,15 @@ import (
 	"os"
 
 	"github.com/urfave/cli"
+
+	"github.com/myth/ulv-auth/oidc"
 )
 
 // NAME : The application name
 const NAME string = "ulv-auth"
 
 // USAGE : Application cli description
-const USAGE string = "Start the ulv-auth OAuth2.0 / OIDC authentication and identity provider server"
+const USAGE string = "OAuth2.0 / OIDC authentication and identity provider server"
 
 // VERSION : current version number
 const VERSION string = "0.1.0"
@@ -21,6 +23,18 @@ func main() {
 	app.Name = NAME
 	app.Usage = USAGE
 	app.Version = VERSION
+
+	app.Commands = []cli.Command{
+		{
+			Name:    "server",
+			Aliases: []string{"s"},
+			Usage:   "Starts the Ulv Auth server",
+			Action: func(c *cli.Context) error {
+				oidc.StartExampleServer()
+				return nil
+			},
+		},
+	}
 
 	app.Run(os.Args)
 }
